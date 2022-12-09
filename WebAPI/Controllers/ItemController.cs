b65afc8b585930965ec56af81e1df45866a59158
spam <andersensimon787@gmail.com>
2022-12-09 17:07:35 +0100

    ItemController.cs

--- WebAPI/Controllers/ItemController.cs
+++ WebAPI/Controllers/ItemController.cs
@@ -1,3 +1,4 @@
+using System.Runtime.ExceptionServices;
 using Logic.LogicInterfaces;
 using Microsoft.AspNetCore.Mvc;
 using Shared.DTOs;
@@ -12,7 +13,6 @@ namespace WebAPI.Controllers;
 public class ItemController : ControllerBase {
 
     private IItemLogic _itemLogic;
-    private IItemManager itemManager;
 
 
     public ItemController(IItemLogic itemLogic) {
@@ -38,22 +38,14 @@ public class ItemController : ControllerBase {
     
     
     [HttpPost]
-    public async Task<ActionResult<Item>> ReserveItem(ItemCreationDto dto) {
-        try {
+    public async void ReserveItem(ItemCreationDto dto) {
+        try
+        {
             Item item = await _itemLogic.CreateAsync(dto);
-            if (item.Type.Id!=dto.ItemTypeId)
-            {
-                return StatusCode(428, "ItemType not found");
-            }
-            return Created($"/item/{item.Uid}", item);
         }
-        catch (Exception e) {
-            Console.WriteLine(e);
-            return StatusCode(500, e.Message);
+        catch
+        {
+            ExceptionDispatchInfo.SetCurrentStackTrace(null);
         }
-        
-        
-        
     }
-    
 }
